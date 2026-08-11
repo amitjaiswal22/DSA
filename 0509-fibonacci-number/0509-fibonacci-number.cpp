e053216df3af77c6c89ade1c22d2b46754b09e50
amitjaiswal22 <114880035+amitjaiswal22@users.noreply.github.com>
2026-08-11 20:17:39 +0530

    Time: 2 ms (77.77%), Space: 7.9 MB (38.93%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int n) {
+        int prev0=0;
+        int prev1=1;
+        if(n==0)
+           return 0;
+           if (n==1){
+              return 1;
+           }
+          for(int i=2;i<=n;i++){
+               int curr=prev0+prev1;
+                prev0=prev1;
+                prev1=curr;
+          } 
+          return prev1;
+    }
+};
