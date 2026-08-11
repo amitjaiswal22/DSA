8ffec126621ff4fca5d7baeae518f9492ba70898
amitjaiswal22 <114880035+amitjaiswal22@users.noreply.github.com>
2026-08-11 21:05:43 +0530

    Time: 0 ms (100%), Space: 10.6 MB (47.25%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -10,6 +10,7 @@ public:
         return max(take,not_take);
    }
    */
+  /* Memoization
     int solve(int i,int n,vector<int>&num,unordered_map<int,int>&mp){
         if(i>=n){
             return 0;
@@ -19,12 +20,20 @@ public:
         int take =num[i]+solve(i+2,n,num,mp);
         int not_take=solve(i+1,n,num,mp);
         return mp[i]=max(take,not_take);
-   }
-    int rob(vector<int>& nums) {
-       int n=nums.size();
-       unordered_map<int,int>mp,dp;
+   }*/
+    int rob(vector<int>& num) {
+       int n=num.size();
+      /* unordered_map<int,int>mp,dp;
        int ans=solve(0,n,nums,mp);
        int ans2=solve(1,n,nums,dp);
-       return max(ans,ans2); 
+       return max(ans,ans2);*/ 
+       if(n==1)
+          return num[0];
+       vector<int>dp(n+2,0);
+       for(int i=n-1;i>=0;i--){
+           dp[i]=max(dp[i+1],dp[i+2]+num[i]);
+           
+       }
+       return max(dp[0],dp[1]);
     }
 };
