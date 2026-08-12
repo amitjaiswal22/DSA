6ba9be44d0d39b15917f796c8e6973f89de7ce22
amitjaiswal22 <114880035+amitjaiswal22@users.noreply.github.com>
2026-08-12 13:33:25 +0530

    Time: 0 ms (100%), Space: 8.7 MB (64.6%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+   
     int solve(int i,int n,string&s,unordered_map<int,long long>&mp){
     if(i>=n)
         return 1;
@@ -19,8 +20,22 @@ public:
     }
     int numDecodings(string s) {
         int n=s.size();
-        unordered_map<int,long long >mp;
+       /* unordered_map<int,long long >mp;
         int ans=solve(0,n,s,mp);
-        return ans;
+        return ans;*/
+        vector<int>dp(n+1,0);
+        dp[n]=1;
+    
+        for(int i=n-1;i>=0;i--){
+             dp[i]+=dp[i+1];
+               if(s[i]-'0'==0)
+                   dp[i]=0;
+                else   
+                 if(i+1<n)
+                 if((s[i]-'0'==1)||((s[i]-'0'==2)&&(s[i+1]-'0'<=6))) 
+                      dp[i]+=dp[i+2];
+
+        }
+        return dp[0];
     }
 };
