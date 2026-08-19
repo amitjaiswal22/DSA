ad580b715bf5017390c5302d8988fd4a7e377566
amitjaiswal22 <114880035+amitjaiswal22@users.noreply.github.com>
2026-08-19 19:37:17 +0530

    Time: 71 ms (5.13%), Space: 27.6 MB (32.19%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int solve(int i,int j,int n,int m,string&s,string &t,vector<vector<int>>&dp){
+        if(i>=n||j>=m)
+             return 0;
+         if(dp[i][j]!=-1)
+            return dp[i][j];    
+        int take=0;
+           if(s[i]==t[j]){
+               take=1+solve(i+1,j+1,n,m,s,t,dp);
+
+           }   
+         int take_i=solve(i+1,j,n,m,s,t,dp);
+         int take_j=solve(i,j+1,n,m,s,t,dp);
+         return dp[i][j]=max(take,max(take_i,take_j));    
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.size();
+        int m=text2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        int ans=solve(0,0,n,m,text1,text2,dp);
+        return ans;
+    }
+};
