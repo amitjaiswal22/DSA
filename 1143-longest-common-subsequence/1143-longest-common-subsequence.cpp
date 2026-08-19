bfa20ea3d56507a3ca8e5dc2423f2848f8bfb2ae
amitjaiswal22 <114880035+amitjaiswal22@users.noreply.github.com>
2026-08-19 19:42:14 +0530

    Time: 26 ms (65.71%), Space: 27.4 MB (70.78%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -17,8 +17,20 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        int ans=solve(0,0,n,m,text1,text2,dp);
-        return ans;
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        /*int ans=solve(0,0,n,m,text1,text2,dp);
+        return ans;*/
+       for(int i=n-1;i>=0;i--){
+         for(int j=m-1;j>=0;j--){
+            int take=0;
+           if(text1[i]==text2[j]){
+               take=1+dp[i+1][j+1];
+           }   
+         int take_i=dp[i+1][j];
+         int take_j=dp[i][j+1];
+         dp[i][j]=max(take,max(take_i,take_j)); 
+         }
+       } 
+       return dp[0][0];
     }
 };
