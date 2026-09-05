ae09bfd7c4f2e6bbf17252d07b962a99b8942e20
amitjaiswal22 <114880035+amitjaiswal22@users.noreply.github.com>
2026-09-05 18:19:51 +0530

    Time: 384 ms (29.9%), Space: 229 MB (19.77%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -1,31 +1,33 @@
 class Solution {
-public:
- vector<int> child[100001];
-    int ans;
-    int dfs(string &s,int curr){
-        if(child[curr].empty())
-          return 1;
-       int a=0,b=0;
-       for(auto v:child[curr]){
-        int len=dfs(s,v);
-        ans=max(ans,len);
-        if(s[curr]==s[v])
-           continue;
-         if(len>a){
-            b=a;
-            a=len;
-         }  
-         else
-             b=max(b,len);
-       }   
-       ans=max(ans,1+a+b);
-       return 1+a;
+public:int ans=0; 
+    int  solve(unordered_map<int,vector<int>>&adj,int i,string &s ){
+        int l=0;
+        int m=0;
+        for(auto v:adj[i]){
+             int child=solve(adj,v,s);
+             if(s[v]==s[i])
+                continue;
+             if(child>l){
+                m=l;
+                l=child;
+             }        
+             else{
+                if(child>m)
+                     m=child;
+             }
+               
+        }
+        ans=max(ans,1+l+m);
+        return 1+l;
+        
     }
     int longestPath(vector<int>& parent, string s) {
-         int n = parent.size();
-        for(int i=1;i<n;i++)child[parent[i]].push_back(i);
-         ans = 1;
-        dfs(s,0);
+        unordered_map<int,vector<int>>adj;
+        int n=parent.size();
+        for(int i=1;i<n;i++){
+              adj[parent[i]].push_back(i);
+        }
+        solve(adj,0,s);
         return ans;
     }
 };
