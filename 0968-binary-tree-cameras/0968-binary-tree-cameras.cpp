03b6eac2f881d5a651565924bb494f90bc8262d3
amitjaiswal22 <114880035+amitjaiswal22@users.noreply.github.com>
2026-09-05 15:11:19 +0530

    Time: 0 ms (100%), Space: 21.4 MB (68.08%) - LeetHub

--- 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
@@ -1,42 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-public:
+public:int res=0;
+    int solve(TreeNode* root){
+        if(root==nullptr)
+             return 2;
 
-    int cameras = 0;
-
-    // 0 -> not covered
-    // 1 -> camera
-    // 2 -> covered
-
-    int solve(TreeNode* root) {
-
-        if (root == nullptr)
-            return 2;
-
-        int left = solve(root->left);
-        int right = solve(root->right);
-
-        // If any child is not covered,
-        // current node must have camera.
-        if (left == 0 || right == 0) {
-            cameras++;
-            return 1;
-        }
-
-        // If any child has camera,
-        // current node is covered.
-        if (left == 1 || right == 1)
-            return 2;
-
-        // Both children are covered but
-        // neither has camera.
-        return 0;
+        int left=solve(root->left);
+        int right=solve(root->right);
+        if(left==0||right==0){
+             res++;return 1;  
+        } 
+          if(left==1||right==1)
+               return 2;
+               return 0;      
     }
-
     int minCameraCover(TreeNode* root) {
-
-        if (solve(root) == 0)
-            cameras++;
-
-        return cameras;
+        if(solve(root)==0)
+               res++;
+        return res;
     }
 };
